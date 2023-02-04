599b77a6d361bf1316e21289844e2f32309ad3f4
Татьяна <brantowatanya@yandex.ru>
2023-02-05 02:53:13 +0300

    corrected task6

--- Task6.py
+++ Task6.py
@@ -8,7 +8,7 @@
 # 385916 -> yes
 # 123456 -> no
 
-ticket = int(input('Введите номер билета: '))
+ticket = int(input('Введите 6-ти значный номер билета: '))
 
 if int(ticket%10) + int((ticket/10)%10) + int((ticket/100)%10) == int(ticket/100000) + int((ticket/1000)%10) + int((ticket/10000)%10):
  print(ticket, '-> yes')
